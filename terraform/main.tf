e6fe6789137ea30e3df186457384a614e4cbc6bd
guillaume <guillaume.belanger27@gmail.com>
2023-07-21 13:06:48 -0400

    Uses basic main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,57 +1,13 @@
 terraform {
-  required_providers {
-    juju = {
-      version = "~> 0.3.1"
-      source  = "juju/juju"
-    }
-  }
+  required_version = ">= 0.12.26"
 }
 
-provider "juju" {}
-
-resource "juju_model" "development" {
-  name = "development"
-
-  cloud {
-    name   = "google"
-    region = "us-central1-c"
-  }
+variable "subject" {
+   type = string
+   default = "World"
+   description = "Subject to hello"
 }
 
-resource "juju_application" "wordpress" {
-  name = "wordpress"
-
-  model = juju_model.development.name
-
-  charm {
-    name = "wordpress"
-  }
-
-  units = 3
-}
-
-resource "juju_application" "percona-cluster" {
-  name = "percona-cluster"
-
-  model = juju_model.development.name
-
-  charm {
-    name = "percona-cluster"
-  }
-
-  units = 3
-}
-
-resource "juju_integration" "wp_to_percona" {
-  model = juju_model.development.name
-
-  application {
-    name     = juju_application.wordpress.name
-    endpoint = "db"
-  }
-
-  application {
-    name     = juju_application.percona-cluster.name
-    endpoint = "server"
-  }
+output "hello_world" {
+  value = "Hello branch demo with source change, ${var.subject}!"
 }
