f78434356c9fe6b4ed7b32ae6ed2c99ca9b3c1a2
guillaume <guillaume.belanger27@gmail.com>
2023-07-21 14:15:23 -0400

    Fixes Juju creds

--- terraform/main.tf
+++ terraform/main.tf
@@ -8,43 +8,39 @@ terraform {
   }
 }
 
-# This is a fully configured provider containing all the
-# relevant information to connect to a Juju controller.
-# This information can be collected by checking the local
-# juju folder (~/.local/share/juju/), or by querying the
-# controller using `juju show-controller --show-password`.
-# If you have already installed and configured a local
-# Juju CLI or prefer to use a configuration using environment
-# variables, you can use an empty provider. See the next
-# example for more details.
+provider "juju" {
+  controller_addresses = "35.225.17.109:17070"
 
-#provider "juju" {
-#  controller_addresses = "10.225.205.241:17070,10.225.205.242:17070"
-#
-#  username = "jujuuser"
-#  password = "password1"
-#
-#  ca_certificate = file("~/ca-cert.pem")
-#}
+  username = "terraform"
+  password = "password123"
 
-
-# When an empty provider is indicated, the Juju
-# provider automatically sets the corresponding values
-# by checking:
-# **First**: the following environment variables that correspond
-# to the configuration fields indicated above.
-# JUJU_CONTROLLER_ADDRESSES
-# JUJU_USERNAME
-# JUJU_PASSWORD
-# JUJU_CA_CERT
-# **Second**: by using a locally installed Juju CLI client.
-# This is the most straight-forward solution. Remember, that
-# it will use the configuration used by the CLI at that
-# moment. The fields are populated using the output
-# from running the command:
-# `juju show-controller --show-password`
-
-provider "juju" {}
+  ca_certificate = <<EOT
+-----BEGIN CERTIFICATE-----
+MIIEEzCCAnugAwIBAgIVAPf93RlyLKjNnh8xqwdPY/V656+DMA0GCSqGSIb3DQEB
+CwUAMCExDTALBgNVBAoTBEp1anUxEDAOBgNVBAMTB2p1anUtY2EwHhcNMjMwNzIx
+MTczODE0WhcNMzMwNzIxMTc0MzE0WjAhMQ0wCwYDVQQKEwRKdWp1MRAwDgYDVQQD
+EwdqdWp1LWNhMIIBojANBgkqhkiG9w0BAQEFAAOCAY8AMIIBigKCAYEA3fu7JgC/
+XbQ9V8cqd6hY27VVAKYVSP5R17MmBBUgeS0UupPKUMxNhUKMHXa8iqQ64BoySa2W
+DyQ+3mWKaXOUTbJewB/rkna6Gf0HL2pXy/1vptc9OdUs4Qi/2UNmAojSo107TSTA
+DGoX6Tbnc+5WJ/KQjn0AWo1htDlvP3NnZagd1nc3km7lGeofLt02cqs/tf8E4vcP
+9Xidng625eI+ElcfizQQzWTD3la4R5waZ9RdOiy0sZfMCzhmEgVMDW1KukaFQ24G
+VnhdgICk1THiHJtiFBZYp5evSl1f6tKm/x6PnRhiqov5qq51ZecLyGIwjuRkuobx
+8mKMgjO/YPeTJWE7SIWMBxALPupGFcoIeR4gtkZMMA9jMt7B1VRuQu8bF1fv3smu
+FwM7MOrPWnrFyLvOPSPbag4y3wsCGeeeNZqZBZduIjyaNzXO6BMXBy84trAE1THA
+lYk4BksnXl3b8AKt5Cfr/sb+XCTx5DmGr15QrpZ9wZc3R1t62ION4JL1AgMBAAGj
+QjBAMA4GA1UdDwEB/wQEAwICpDAPBgNVHRMBAf8EBTADAQH/MB0GA1UdDgQWBBTH
+TIxD9R7VbAYkom4Mj8SwdeG83zANBgkqhkiG9w0BAQsFAAOCAYEATxhcRw7maQlg
+ei2rRrbgeorbDJRnkTqLGrG7E6TD2yQQS1zyJSRxGPkrvM67rAVabIfYVtO1PFf7
+1xg9Yoq3xqEUrEPvVX2HLCv8ZHwctpi9D0tSFNdJR2zkXMaTMkxcWgJnAJq5//+6
+R96q7Uwv/2QShWhjmSk9qxhycOxZnTzHNnnBIT2DjyXl8lvMQezgn8sA/vXKdHEZ
+tmq904TxK1AgLLCbvYZ/4SZHgHDkC/xLsEgC2K+iPIShvg9hCMtN76h/Adhjnw+G
+2HIjx4VDzzenqn/nx81Fs3Gmpo9XNoCsEKFBXdoTKQ5otalMAqaNJ8U3jG1AbKDC
+CAmje38QmDCqWTu+QpRnISDxjan+uSOvh+0u1lZ8VxjweH6vq/0QIq+ZtMTokj++
+P8bn47mWGj0qRSvQEn0eEW/lNDkjUcSfs4EoylUPKMbUddBO4VWQYJxQ6tb3cGTI
+Yx+aWfvi6YlEGSFZoDMDlCEa065fWviV7TCdFvqsuiGvwzblx/R5
+-----END CERTIFICATE-----
+EOT
+}
 
 resource "juju_model" "development" {
   name = "development"
