e609e2aa978ac285ef9f3bb6930f5c2a62854997
guillaume <guillaume.belanger27@gmail.com>
2023-07-27 19:29:31 -0400

    Bug fix

--- terraform/cos-eks/main.tf
+++ terraform/cos-eks/main.tf
@@ -8,7 +8,12 @@ provider "aws" {
 data "aws_availability_zones" "available" {}
 
 locals {
-  cluster_name = "cos-eks"
+  cluster_name = "education-eks-${random_string.suffix.result}"
+}
+
+resource "random_string" "suffix" {
+  length  = 8
+  special = false
 }
 
 module "vpc" {
