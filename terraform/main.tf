48c6f6d40ee8328d1f8f3e00a2b915f421b4249c
guillaume <guillaume.belanger27@gmail.com>
2023-07-27 10:20:06 -0400

    Bug fix

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,21 +3,6 @@ module "kubernetes" {
   source = "./kubernetes"
 }
 
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "4.52.0"
-    }
-  }
-
-  backend "s3" {
-   bucket = "tfstate-gruyaume"
-   region = "us-east-2"
-   key    = "terraform.tfstate"
- }
-}
-
 provider "aws" {
   region = "us-east-2"
 }
