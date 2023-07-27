c366509400dc0e442aeb683846f7c73192924c13
guillaume <guillaume.belanger27@gmail.com>
2023-07-27 19:28:00 -0400

    Bug fix

--- terraform/outputs.tf
+++
@@ -1,7 +0,0 @@
-# Copyright (c) HashiCorp, Inc.
-# SPDX-License-Identifier: MPL-2.0
-
-output "cluster_endpoint" {
-  description = "Endpoint for EKS control plane"
-  value       = module.cos_eks.cluster_endpoint
-}
