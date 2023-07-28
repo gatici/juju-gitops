54359f28f80610175d23a3cfddabe4596d9b4c58
guillaume <guillaume.belanger27@gmail.com>
2023-07-28 09:46:43 -0400

    Bug fix

--- terraform/cos/juju/main.tf
+++ terraform/cos/juju/main.tf
@@ -14,72 +14,72 @@ resource "juju_model" "cos" {
 
   cloud {
     name   = "cos-eks"
-    region = "es-east-2"
+    region = "us-east-2"
   }
 }
-#
-#resource "juju_application" "grafana" {
-#  name = "grafana"
-#
-#  model = juju_model.cos.name
-#
-#  charm {
-#    name = "grafana-k8s"
-#  }
-#
-#  units = 1
-#}
-#
-#resource "juju_application" "prometheus" {
-#  name = "prometheus"
-#
-#  model = juju_model.cos.name
-#
-#  charm {
-#    name = "prometheus-k8s"
-#  }
-#
-#  units = 1
-#}
-#
-#resource "juju_integration" "grafana_source" {
-#  model = juju_model.cos.name
-#
-#  application {
-#    name     = juju_application.grafana.name
-#    endpoint = "grafana-source"
-#  }
-#
-#  application {
-#    name     = juju_application.prometheus.name
-#    endpoint = "grafana-source"
-#  }
-#}
-#
-#resource "juju_integration" "metrics" {
-#  model = juju_model.cos.name
-#
-#  application {
-#    name     = juju_application.grafana.name
-#    endpoint = "metrics-endpoint"
-#  }
-#
-#  application {
-#    name     = juju_application.prometheus.name
-#    endpoint = "metrics-endpoint"
-#  }
-#}
-#
-#resource "juju_integration" "dashboard" {
-#  model = juju_model.cos.name
-#
-#  application {
-#    name     = juju_application.grafana.name
-#    endpoint = "grafana-dashboard"
-#  }
-#
-#  application {
-#    name     = juju_application.prometheus.name
-#    endpoint = "grafana-dashboard"
-#  }
-#}
+
+resource "juju_application" "grafana" {
+  name = "grafana"
+
+  model = juju_model.cos.name
+
+  charm {
+    name = "grafana-k8s"
+  }
+
+  units = 1
+}
+
+resource "juju_application" "prometheus" {
+  name = "prometheus"
+
+  model = juju_model.cos.name
+
+  charm {
+    name = "prometheus-k8s"
+  }
+
+  units = 1
+}
+
+resource "juju_integration" "grafana_source" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.grafana.name
+    endpoint = "grafana-source"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "grafana-source"
+  }
+}
+
+resource "juju_integration" "metrics" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.grafana.name
+    endpoint = "metrics-endpoint"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "metrics-endpoint"
+  }
+}
+
+resource "juju_integration" "dashboard" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.grafana.name
+    endpoint = "grafana-dashboard"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "grafana-dashboard"
+  }
+}
