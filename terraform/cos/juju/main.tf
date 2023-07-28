0d59e89630b0d455a64f908a833dd17cb35f6a2e
guillaume <guillaume.belanger27@gmail.com>
2023-07-28 10:14:18 -0400

    Adds integrations

--- terraform/cos/juju/main.tf
+++ terraform/cos/juju/main.tf
@@ -42,45 +42,79 @@ resource "juju_application" "prometheus" {
 
   units = 1
 }
-#
-#resource "juju_integration" "grafana_source" {
-#  model = juju_model.cos.name
-#
-#  application {
-#    name     = juju_application.grafana.name
-#    endpoint = "grafana-source"
-#  }
-#
-#  application {
-#    name     = juju_application.prometheus.name
-#    endpoint = "grafana-source"
-#  }
-#}
-#
-#resource "juju_integration" "metrics" {
-#  model = juju_model.cos.name
-#
-#  application {
-#    name     = juju_application.grafana.name
-#    endpoint = "metrics-endpoint"
-#  }
-#
-#  application {
-#    name     = juju_application.prometheus.name
-#    endpoint = "metrics-endpoint"
-#  }
-#}
-#
-#resource "juju_integration" "dashboard" {
-#  model = juju_model.cos.name
-#
-#  application {
-#    name     = juju_application.grafana.name
-#    endpoint = "grafana-dashboard"
-#  }
-#
-#  application {
-#    name     = juju_application.prometheus.name
-#    endpoint = "grafana-dashboard"
-#  }
-#}
+
+resource "juju_application" "traefik" {
+  name = "traefik"
+  model = juju_model.cos.name
+  trust = true
+
+  charm {
+    name = "traefik-k8s"
+    channel  = "latest/stable"
+  }
+
+  units = 1
+}
+
+resource "juju_integration" "grafana_source" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.grafana.name
+    endpoint = "grafana-source"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "grafana-source"
+  }
+}
+
+resource "juju_integration" "metrics" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.grafana.name
+    endpoint = "metrics-endpoint"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "metrics-endpoint"
+  }
+}
+
+resource "juju_integration" "dashboard" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.grafana.name
+    endpoint = "grafana-dashboard"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "grafana-dashboard"
+  }
+}
+
+resource "juju_integration" "grafana_ingress" {
+  model = juju_model.cos.name
+
+  application {
+    name     = juju_application.traefik.name
+    endpoint = "ingress-per-unit"
+  }
+
+  application {
+    name     = juju_application.prometheus.name
+    endpoint = "ingress"
+  }
+}
+
+
+#- [traefik:traefik-route, grafana:ingress]
+#- [grafana:grafana-source, prometheus:grafana-source]
+#- [prometheus:metrics-endpoint, traefik:metrics-endpoint]
+#- [prometheus:metrics-endpoint, grafana:metrics-endpoint]
+#- [grafana:grafana-dashboard, prometheus:grafana-dashboard]
