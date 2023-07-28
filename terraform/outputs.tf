752b7d2352513c8c56fcd09aa1ca9ab2005d7882
guillaume <guillaume.belanger27@gmail.com>
2023-07-28 10:58:15 -0400

    Adds mattermost EKS cluster

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -2,8 +2,8 @@ output "cos_cluster_name" {
   description = "COS Kubernetes cluster name"
   value       = module.cos.cluster_name
 }
-#
-#output "mattermost_cluster_name" {
-#  description = "Mattermost Kubernetes cluster name"
-#  value       = module.mattermost.cluster_name
-#}
+
+output "mattermost_cluster_name" {
+  description = "Mattermost Kubernetes cluster name"
+  value       = module.mattermost.cluster_name
+}
