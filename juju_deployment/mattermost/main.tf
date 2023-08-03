186e78be94457818ea623790e12913c7ee6bf920
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-08-03 07:40:48 -0400

    Adds certificates relation

--- juju_deployment/mattermost/main.tf
+++ juju_deployment/mattermost/main.tf
@@ -44,6 +44,18 @@ resource "juju_application" "postgresql" {
   units = 1
 }
 
+resource "juju_application" "self_signed_certs" {
+  name = "self-signed-certificates"
+  model = juju_model.mattermost.name
+
+  charm {
+    name = "self-signed-certificates"
+    channel  = "beta"
+  }
+
+  units = 1
+}
+
 resource "juju_integration" "db" {
   model = juju_model.mattermost.name
 
@@ -58,16 +70,30 @@ resource "juju_integration" "db" {
   }
 }
 
-
-resource "juju_integration" "metrics" {
+resource "juju_integration" "tls" {
   model = juju_model.mattermost.name
 
   application {
-    name     = juju_application.postgresql.name
-    endpoint = "metrics-endpoint"
+    name     = juju_application.self_signed_certs.name
+    endpoint = "certificates"
   }
 
   application {
-    offer_url = var.metrics_offer_url
+    name     = juju_application.postgresql.name
+    endpoint = "certificates"
   }
 }
+
+
+# resource "juju_integration" "metrics" {
+#   model = juju_model.mattermost.name
+#
+#   application {
+#     name     = juju_application.postgresql.name
+#     endpoint = "metrics-endpoint"
+#   }
+#
+#   application {
+#     offer_url = var.metrics_offer_url
+#   }
+# }
