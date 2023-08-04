55e0beb8db51ede58d38c5c44cb3efa03c5ab3d8
Guillaume Belanger <guillaume.belanger@canonical.com>
2023-08-03 20:52:25 -0400

    Deletes COS and SD-Core deployments

--- juju_deployment/main.tf
+++ juju_deployment/main.tf
@@ -1,14 +1,14 @@
-module "cos" {
-  source = "./cos"
-  cloud_name = "eks_1"
-}
-
-module "sdcore_cp" {
-  source = "./sdcore_cp"
-  cloud_name = "eks_2"
-  metrics_remote_write_offer_url = module.cos.metrics_remote_write_offer_url
-  logging_offer_url = module.cos.logging_offer_url
-}
+# module "cos" {
+#   source = "./cos"
+#   cloud_name = "eks_1"
+# }
+#
+# module "sdcore_cp" {
+#   source = "./sdcore_cp"
+#   cloud_name = "eks_2"
+#   metrics_remote_write_offer_url = module.cos.metrics_remote_write_offer_url
+#   logging_offer_url = module.cos.logging_offer_url
+# }
 
 # module "sdcore_up" {
 #   source = "./sdcore_up"
